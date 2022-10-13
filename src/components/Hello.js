93e90c785f12f795a12b1033fb918b8d2f3d905c
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-10-13 11:54:25 -0400

    created a Hello World react app

---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hello() {
+  const message = 'Hello world!';
+  return (
+    <div className="container">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default Hello;
